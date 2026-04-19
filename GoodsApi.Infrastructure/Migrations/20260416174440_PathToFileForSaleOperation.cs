ba3ec86e5bba6ed80954884162fc34122ff8e0a5
Fool <Fool@ex.com>
2026-04-19 20:15:40 +0300

    update: миграции и шаблон для чека

---
+++ GoodsApi.Infrastructure/Migrations/20260416174440_PathToFileForSaleOperation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoodsApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PathToFileForSaleOperation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PathToFile",
+                table: "sale_operations",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PathToFile",
+                table: "sale_operations");
+        }
+    }
+}
---
+++ GoodsApi.Infrastructure/Migrations/20260417202036_PathToFileNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoodsApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PathToFileNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PathToFile",
+                table: "sale_operations",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PathToFile",
+                table: "sale_operations",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
